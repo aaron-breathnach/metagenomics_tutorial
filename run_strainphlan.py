63eeaa76837ec917b1c2a458e1a0fec972fa0c67
aaron-breathnach <61203192+aaron-breathnach@users.noreply.github.com>
2026-05-12 13:00:57 +0100

    fixed strainphlan script

---
+++ run_strainphlan.py
@@ -0,0 +1,47 @@
+
+import os
+from pathlib import Path
+
+sample2markers = '''sample2markers.py \
+-i {bam} \
+-o {consensus_markers} \
+-d {pkl} \
+-f bam \
+--nproc {threads}'''
+
+extract_markers = '''extract_markers.py \
+-d {pkl} \
+-c {clade} \
+-o {clade_markers}'''
+
+strainphlan = '''strainphlan \
+-s output/metaphlan/consensus_markers/* \
+-m {clade_markers}/{clade}.fna \
+-o {output} \
+-c {clade} \
+--phylophlan_mode fast \
+--nproc {threads} \
+-d {pkl}'''
+
+pkl = '/data/Food/analysis/R2560_prepop/klara/databases/metaphlan/mpa_vJan25_CHOCOPhlAnSGB_202503/mpa_vJan25_CHOCOPhlAnSGB_202503.pkl'
+clade = 't__SGB7044'
+threads = 8
+
+out_dirs = ['output/metaphlan/consensus_markers', 'output/metaphlan/clade_markers', 'output/metaphlan/strainphlan']
+for out_dir in out_dirs:
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+bams = [x.as_posix() for x in Path('output/metaphlan/sam').rglob('*.bam')]
+
+cmds = []
+for bam in bams:
+    cmd = sample2markers.format(bam=bam, consensus_markers=out_dirs[0], pkl=pkl, threads=threads)
+    cmds.append(cmd)
+
+cmds.append(extract_markers.format(pkl=pkl, clade=clade, clade_markers=out_dirs[1]))
+cmds.append(strainphlan.format(clade_markers=out_dirs[1], clade=clade, output=out_dirs[2], threads=threads, pkl=pkl))
+
+with open(f'scripts/run_strainphlan_{clade}.sh', 'w') as f:
+    for cmd in cmds:
+        f.write(cmd + '\n')
